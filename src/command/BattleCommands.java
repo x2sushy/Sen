60e213a18193039cde43471a295e51df32614e7d
x2sushy <kopacek@gmail.com>
2026-01-10 15:24:00 +0100

    added classes MoveCommands, BattleCommands and OtherCommands and made them extend Command

---
+++ src/command/BattleCommands.java
@@ -0,0 +1,13 @@
+package command;
+
+public class BattleCommands extends Command {
+    @Override
+    public String execute() {
+        return "";
+    }
+
+    @Override
+    public boolean exit() {
+        return false;
+    }
+}
---
+++ src/command/OtherCommands.java
@@ -0,0 +1,13 @@
+package command;
+
+public class OtherCommands extends Command {
+    @Override
+    public String execute() {
+        return "";
+    }
+
+    @Override
+    public boolean exit() {
+        return false;
+    }
+}
