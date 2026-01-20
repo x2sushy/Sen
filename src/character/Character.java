fe9344f5524845b2b3e7684882e5c34a41d2b8ff
x2sushy <kopacekd@gmail.com>
2026-01-20 20:56:30 +0100

    commited just because idea isn´t working

--- src/character/Character.java
+++ src/character/Character.java
@@ -10,12 +10,40 @@ public abstract class Character {
     protected String name;
     protected ArrayList<Item> loot;
 
-    public Character(int health, String name) {
-        this.health = health;
-        this.name = name;
-        this.loot = new ArrayList<>();
+    public Character() {
     }
     public void attack(){
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Item> getLoot() {
+        return loot;
+    }
+
+    public void setLoot(ArrayList<Item> loot) {
+        this.loot = loot;
     }
 }
--- src/character/SC.java
+++ src/character/SC.java
@@ -4,8 +4,7 @@ public class SC extends Character{
 
     private final boolean isFriendly;
 
-    public SC(int health, String name, boolean isFriendly) {
-        super(health, name);
-        this.isFriendly = isFriendly;
+    public SC() {
     }
+
 }
